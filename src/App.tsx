6c8dc180a5ce0850b70e14a4a5061fd5d97a0d39
unknown <drmeloy@gmail.com>
2024-01-14 23:58:45 -0800

    can now look up match date and length on click on match

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-import { UserDataResponse, getAccountPuuid } from './services/services';
+import { MatchData, getAccountPuuid, getDetails, getMatches } from './services/services';
 
 function App() {
   const [summonerName, setSummonerName] = useState('');
   const [puuid, setPuuid] = useState('');
-  const [matchHistory, setMatchHistory] = useState([]);
+  const [matchHistory, setMatchHistory] = useState<string[]>([]);
+  const [matchData, setMatchData] = useState<MatchData>();
+  const [matchLength, setMatchLength] = useState('');
+  const [matchDate, setMatchDate] = useState('');
 
   const getPuuid = async (userName: string) => {
     const acctData = await getAccountPuuid(userName);
     if (acctData.puuid) setPuuid(acctData.puuid);
   }
+
+  const getMatchHistory = async (id: string) => {
+    const matches = await getMatches(id);
+    if (matches.length) setMatchHistory(matches)
+  }
+
+  const getMatchDetails = async (matchId: string) => {
+    const details = await getDetails(matchId);
+    console.log(details)
+    if (details) setMatchData(details)
+  }
+
+  useEffect(() => {
+    if (puuid) getMatchHistory(puuid);
+  }, [puuid]);
+
+  useEffect(() => {
+    if (matchData) {
+      const date = new Date(matchData.info.gameCreation);
+      setMatchDate(date.toDateString());
+      const time = Math.ceil(matchData.info.gameDuration / 60);
+      setMatchLength(`${time} minutes`);
+    }
+  }, [matchData])
 
   return (
     <div className="App">
@@ -19,6 +45,9 @@ function App() {
       <input value={summonerName} onChange={({ target }) => setSummonerName(target.value)} />
       <button onClick={() => getPuuid(summonerName)}>Go</button>
       {puuid && <p>Puuid: {puuid}</p>}
+      {matchHistory.length > 0 && <div>Matches: {matchHistory.map(match => <div key={match} onClick={() => getMatchDetails(match)}>{match}</div>)}</div>}
+      {matchDate && <p>Match date: {matchDate}</p>}
+      {matchLength && <p>Match length: {matchLength}</p>}
     </div>
   );
 }
--- src/services/services.ts
+++ src/services/services.ts
@@ -13,7 +13,23 @@ export type UserDataResponse = {
   "summonerLevel": number;
 }
 
+export type MatchData = {
+  info: {
+    gameCreation: number;
+    gameDuration: number;
+    gameEndTimestamp?: number;
+  }
+}
+
 export const getAccountPuuid = (summonerName: string): Promise<UserDataResponse> => axios.get(`${baseUrl}/summoner/v4/summoners/by-name/${summonerName}?api_key=${authToken}`)
 .then(res => res.data)
 .catch(err => console.log('Err in getAccountPuuid: ', err))
 ;
+
+export const getMatches = (puuid: string): Promise<string[]> => axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?api_key=${authToken}`)
+.then(res => res.data)
+.catch(err => console.log('Err in getMatches: ', err))
+
+export const getDetails = (matchId: string): Promise<MatchData> => axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${authToken}`)
+.then(res => res.data)
+.catch(err => console.log('Err in getDetails: ', err))
